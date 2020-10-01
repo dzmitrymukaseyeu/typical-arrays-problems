8cb4ced0cdb272e75815ca4bf8688bfbee48e81c
Dzmitry Mukaseyeu <dimamukas@gmail.com>
2020-10-01 23:20:41 +0300

    feat: add 3 function for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    return Math.min( ...array );
+  };
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    return Math.max( ...array );
+  };
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!array || !array.length) {
+    return 0;
+  } else {  
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  };
+};
